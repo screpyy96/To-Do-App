4fbfe80963d1540cc2554aa9e7c9932f63ff00c9
Iosif <iosifscrepy@gmail.com>
2021-06-12 23:00:02 +0100

    add to do form

--- src/component/todo/todo.jsx
+++ src/component/todo/todo.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../../store/reducer";
 
-const Todo = () => {
-  return <div></div>;
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
 };
 
-export default Todo;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+const Todo = (props) => {
+  console.log("props", props);
+
+  const [value, setValue] = useState("");
+
+  const addTodo = () => {
+    if (value === "") {
+      alert("input empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        completed: false,
+        value,
+      });
+    }
+    setValue("");
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => handleChange(e)} value={value} />
+      <button type="submit" onClick={() => addTodo()}>
+        Add
+      </button>
+      <ul>
+        {props.todos.length > 0 &&
+          props.todos.map((item) => {
+            return <li key={item.id}>{item.value}</li>;
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Todo);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import Todo from "./component/todo/";
+import { store } from "./store/store";
 
-const app = <Todo />;
+const app = (
+  <Provider store={store}>
+    <Todo />
+  </Provider>
+);
 const here = document.querySelector("#here");
 
 render(app, here);
